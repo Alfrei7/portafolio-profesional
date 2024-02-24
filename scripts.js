0c2fec00e98a2ee8cf5b01ce0bc6c2a2499f1707
Alfrei <alfrecps@gmail.com>
2024-02-24 16:15:31 -0300

    cambios

--- scripts.js
+++ scripts.js
@@ -1,36 +1,74 @@
-function toggleColorMode() {
-    const body = document.body;
-    const isDarkMode = body.classList.toggle('dark-mode');
-    const colorToggle = document.getElementById('colorToggle');
-    const iconsContainer = document.querySelector('#colorToggle .icons-container');
-    const moonIcon = document.querySelector('.moon-icon');
-    const sunIcon = document.querySelector('.sun-icon');
+document.addEventListener('DOMContentLoaded', function() {
+  const sections = document.querySelectorAll('section');
 
-    const lightModeStyles = {
-        backgroundColor: '#669a96',
-        color: 'white',
-        moonIconDisplay: 'none',
-        sunIconDisplay: 'inline'
-    };
+  function fadeOutSection(section) {
+    section.classList.remove('fade-in');
+    section.classList.add('fade-out');
+  }
 
-    const darkModeStyles = {
-        backgroundColor: '#7cbab5',
-        color: '#0d1010',
-        moonIconDisplay: 'inline',
-        sunIconDisplay: 'none'
-    };
+  function fadeInSection(section) {
+    section.classList.remove('fade-out');
+    section.classList.add('fade-in', 'section-enter');
+  }
 
-    const styles = isDarkMode ? darkModeStyles : lightModeStyles;
-    Object.assign(colorToggle.style, styles);
-    moonIcon.style.display = styles.moonIconDisplay;
-    sunIcon.style.display = styles.sunIconDisplay;
+  // Quita la clase fade-out de todas las secciones al cargar la página
+  sections.forEach(function(section) {
+    section.classList.remove('fade-out');
+  });
 
-    // Agrega una animación para el desplazamiento de los iconos al presionar el botón
-    iconsContainer.style.transform = 'translateX(-50%)'; // Inicia desde el centro
-    setTimeout(() => {
-        iconsContainer.style.transform = 'translateX(50%)'; // Desplaza a la derecha
-        setTimeout(() => {
-            iconsContainer.style.transform = 'translateX(0)'; // Vuelve al centro
-        }, 300);
-    }, 50);
+  document.querySelectorAll('nav a').forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      const targetId = this.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+
+      if (targetSection) {
+        sections.forEach(function(section) {
+          if (section !== targetSection) {
+            fadeOutSection(section);
+          }
+        });
+        fadeInSection(targetSection);
+      }
+    });
+  });
+
+  function fadeInSection(section) {
+    section.classList.remove('fade-out', 'section-exit'); // Eliminar la clase section-exit si está presente
+    section.classList.add('fade-in', 'section-enter');
+  }
+
+  // Agrega la animación de entrada y salida de secciones al desplazarse por el sitio
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-intersecting');
+      } else {
+        entry.target.classList.remove('is-intersecting');
+      }
+    });
+  });
+
+  document.querySelectorAll('section').forEach(section => {
+    observer.observe(section);
+  });
+});
+
+// carrusel
+
+let slideIndex = 0;
+
+function moveSlide(n) {
+  const slides = document.querySelector('.slides');
+  const slideWidth = slides.children[0].offsetWidth; // Obtén el ancho de cada diapositiva
+  const maxIndex = slides.children.length - 1;
+
+  slideIndex += n;
+  if (slideIndex > maxIndex) {
+    slideIndex = 0; // Vuelve al principio si se llega al final
+  } else if (slideIndex < 0) {
+    slideIndex = maxIndex; // Ir al final si se va hacia atrás desde el principio
+  }
+
+  slides.style.transform = `translateX(-${slideIndex * slideWidth}px)`; // Mueve el carrusel
 }
